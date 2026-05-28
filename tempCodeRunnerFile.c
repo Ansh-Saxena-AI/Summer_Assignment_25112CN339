342cc81bb5ac02531b16bb530740df8b170c2a9d
Ansh Saxena <anshbvm.9539@gmail.com>
2026-05-28 23:48:52 +0530

    First commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int number, count = 0;
+
+    printf("ENTER THE NUMBER: ");
+    scanf("%d", &number);
+
+    int onum = number;
+
+    if(number == 0)
+    {
+        count = 1;
+    }
+    else
+    {
+        while(number > 0)
+        {
+            count = count + 1;
+            number = number / 10;
+        }
+    }
+
+    printf("THE NUMBER OF DIGITS IN %d IS %d", onum, count);
+
+    return 0;
+}

ef118ad851dc126fa91d9ba19f660a64a26bb056
Ansh-Saxena-AI <anshbvm.9539@gmail.com>
2026-06-21 15:18:33 +0530

    Day-26 Uploaded

---
+++ Day-26/Q102.C
@@ -0,0 +1,17 @@
+//Write a program to Create voting eligibility system.
+
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if (age >= 18)
+        printf("Eligible to vote.\n");
+    else
+        printf("Not eligible to vote.\n");
+
+    return 0;
+}
